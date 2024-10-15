9949afd838833053ee452c06ce08d53f346160ae
BigKawa <164774630+BigKawa@users.noreply.github.com>
2024-10-15 20:38:38 +0200

    Update Lab CTE.sql

--- Lab CTE.sql
+++ Lab CTE.sql
@@ -1,38 +1,40 @@
 USE sakila;
 
+
+DROP VIEW rental_summary;
+
 # Create a VIEW
 
 CREATE VIEW rental_summary AS 
-SELECT customer_id,first_name, last_name, email, COUNT(rental_id) AS total_rentals
+SELECT customer_id,first_name, last_name, email, COUNT(rental_id) AS rental_count
 FROM customer
 INNER JOIN rental
 USING (customer_id)
 GROUP BY customer_id;
 
-SELECT * FROM test;
+SELECT * FROM rental_summary;
 # CREATE CTE
 with payment_summary as (
 SELECT customer_id, SUM(payment.amount) AS total_paid
 FROM rental_summary
 INNER JOIN payment
 USING (customer_id)
-GROUP BY customer_id)
+GROUP BY customer_id),
 
-SELECT * FROM test2;
 
 
 -- Step 3: Create a CTE to join the rental summary view with the payment summary CTE
-with customer_summary AS (
+customer_summary AS (
     SELECT 
         rs.customer_id,
         rs.first_name,
         rs.last_name,
         rs.email,
         rs.rental_count,
-        ps.total_paid,
-        (ps.tota∑l_paid / rs.rental_count) AS average_payment_per_rental
+        payment_summary.total_paid,
+        (payment_summary.total_paid / rs.rental_count) AS average_payment_per_rental
     FROM rental_summary rs
-    INNER JOIN payment_summary ps ON rs.customer_id = ps.customer_id
+    INNER JOIN payment_summary ON rs.customer_id = payment_summary.customer_id
 )
 
 -- Step 4: Generate the final customer summary report
